e5f78e25258341d3c8f9d9f322ba022120744561
esoubiran-aneo <esoubiran@aneo.fr>
2023-01-17 16:56:12 +0100

    fix: add baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,7 @@ import { version } from './package.json'
 logger.success(`Using ArmoniK Docs Theme v${version}`)
 
 export default defineNuxtConfig({
+  app: { baseURL: process.env.NODE_ENV === 'production' ? '/armonik-docs-theme/' : '' },
+
   extends: '@nuxt-themes/docus'
 })
