7fc437950b2aa0073316ed633710c1b6b0d6f4eb
esoubiran-aneo <esoubiran@aneo.fr>
2023-01-18 13:51:52 +0100

    fix: color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,17 @@ import { version } from './package.json'
 logger.success(`Using ArmoniK Docs Theme v${version}`)
 
 export default defineNuxtConfig({
-  app: { baseURL: process.env.NODE_ENV === 'production' ? '/armonik-docs-theme/' : '' },
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/armonik-docs-theme/' : '',
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'favicon', content: '/favicon.ico' },
+      ]
+    }
+  },
 
   extends: '@nuxt-themes/docus'
+
+
 })
