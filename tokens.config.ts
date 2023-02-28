43377c89a5daed412fff0dbe524ec9c0f85d6eef
esoubiran-aneo <esoubiran@aneo.fr>
2023-02-28 14:36:55 +0100

    fix: restore colors fix #18

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,7 +1,18 @@
-import { defineTheme, palette } from 'pinceau'
+import { defineTheme } from 'pinceau'
 
 export default defineTheme({
   color: {
-    primary: palette('orange')
+    primary: {
+      50: '#fff7ed',
+      100: '#ffedd5',
+      200: '#fed7aa',
+      300: '#fdba74',
+      400: '#fb923c',
+      500: '#f97316',
+      600: '#ea580c',
+      700: '#c2410c',
+      800: '#9a3412',
+      900: '#7c2d12',
+    }
   }
 })
