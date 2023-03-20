4870907c32e063487df0dc433f389b15fc264553
esoubiran-aneo <esoubiran@aneo.fr>
2023-03-20 11:31:15 +0100

    fix: favicon fix #21

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,21 @@ import { version } from './package.json'
 
 logger.success(`Using ArmoniK Docs Theme v${version}`)
 
+const baseURL = process.env.NODE_ENV === 'production' ? '/armonik-docs-theme/' : '/'
+
 export default defineNuxtConfig({
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/armonik-docs-theme/' : '',
+    baseURL: baseURL,
     head: {
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'favicon', content: '/favicon.ico' },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/ico',
+          href: `${baseURL}favicon.ico`,
+        }
       ]
     }
   },
